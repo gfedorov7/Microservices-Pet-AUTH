270705526b75fa12260a390831d40cc54a27635c
gfedorov7 <fedorov.programming@gmail.com>
2026-03-07 00:11:18 +0300

    update service

--- src/service/create_service_user.py
+++ src/service/create_service_user.py
@@ -21,17 +21,24 @@ class UserCreateService:
         self.hasher = hasher
 
     async def create_user(self, new_user: Dict[str, Any]) -> UserRead:
-        try:
-            _ = self.new_user_validator.is_valid()
-        except AppException as e:
-            raise e
+        _ = self.new_user_validator.is_valid()
+        password = self._get_password_from_request(new_user)
+        new_user = self.hash_password_and_set_to_object(password, new_user)
+        return await self.create_user_and_formated(new_user)
 
+    @staticmethod
+    def _get_password_from_request(new_user: Dict[str, Any]) -> str:
         password = new_user.pop("password", None)
         if password is None:
             raise AppException(app_errors[ErrorType.PASSWORD_IS_NOT_SPECIFIED])
 
+        return password
+
+    def hash_password_and_set_to_object(self, password: str, new_user: Dict[str, Any]) -> Dict[str, Any]:
         hash_password = self.hasher.hash(password)
         new_user["password"] = hash_password
+        return new_user
 
+    async def create_user_and_formated(self, new_user: Dict[str, Any]) -> UserRead:
         user = await self.user_repo.create(new_user)
         return UserRead(**user)
