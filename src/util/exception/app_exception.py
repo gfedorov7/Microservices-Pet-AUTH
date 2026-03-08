cfd9bcc9a92ed9eebf79df2eab587f4bed79b694
gfedorov7 <fedorov.programming@gmail.com>
2026-03-08 17:57:44 +0300

    fix problem with name

--- src/util/exception/app_exception.py
+++ src/util/exception/app_exception.py
@@ -4,5 +4,5 @@ from src.util.errors.app_error import AppError
 class AppException(Exception):
     def __init__(self, app_error: AppError):
         self.message = app_error.message
-        self.status_code = app_error.status_code
+        self.status_code = app_error.code
         super().__init__(self.message)
