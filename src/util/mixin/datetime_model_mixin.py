9c175d0776d2530383c36db8cbd20ded3a45625d
gfedorov7 <fedorov.programming@gmail.com>
2026-03-08 17:57:04 +0300

    delete moscow timezone

--- src/util/mixin/datetime_model_mixin.py
+++ src/util/mixin/datetime_model_mixin.py
@@ -4,12 +4,7 @@ from zoneinfo import ZoneInfo
 from sqlalchemy.orm import Mapped, mapped_column
 
 
-moscow_tz = ZoneInfo("Europe/Moscow")
-
-def now_msk_naive() -> datetime:
-    return datetime.now(moscow_tz).replace(tzinfo=None)
-
 class DateTimeModelMixin:
-    created_at: Mapped[datetime] = mapped_column(default=now_msk_naive)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
 
-    updated_at: Mapped[datetime] = mapped_column(default=now_msk_naive, onupdate=now_msk_naive)
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(), onupdate=datetime.now())
