d1eebafef0675e0e72d4b47e36e76576982a9745
gfedorov7 <fedorov.programming@gmail.com>
2026-03-08 17:57:22 +0300

    update main for start project for cors, origins and exceptions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
+from src.api import api_router
+from src.util.exception.app_exception import AppException
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.message},
+    )
