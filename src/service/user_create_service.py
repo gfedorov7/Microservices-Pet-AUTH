c4d5264dfd300e30bccda26c15dfc47f5dec6081
gfedorov7 <fedorov.programming@gmail.com>
2026-03-08 17:53:51 +0300

    rename and fix service user

--- src/service/user_create_service.py
+++ src/service/user_create_service.py
@@ -13,32 +13,30 @@ class UserCreateService:
     def __init__(
             self,
             user_repo: UserRepository,
-            new_user_validator: Validator,
             hasher: Hasher
     ):
         self.user_repo = user_repo
-        self.new_user_validator = new_user_validator
         self.hasher = hasher
 
-    async def create_user(self, new_user: Dict[str, Any]) -> UserRead:
-        _ = self.new_user_validator.is_valid()
+    async def create_user(self, new_user: Dict[str, Any], validator: Validator) -> UserRead:
+        _ = await validator.is_valid()
         password = self._get_password_from_request(new_user)
-        new_user = self.hash_password_and_set_to_object(password, new_user)
-        return await self.create_user_and_formated(new_user)
+        new_user = self._hash_password_and_set_to_object(password, new_user)
+        return await self._create_user_and_formated(new_user)
 
     @staticmethod
-    def _get_password_from_request(new_user: Dict[str, Any]) -> str:
+    def _get_password_from_request(new_user: Dict[str, Any]) -> str | None:
         password = new_user.pop("password", None)
         if password is None:
             raise AppException(app_errors[ErrorType.PASSWORD_IS_NOT_SPECIFIED])
 
         return password
 
-    def hash_password_and_set_to_object(self, password: str, new_user: Dict[str, Any]) -> Dict[str, Any]:
+    def _hash_password_and_set_to_object(self, password: str, new_user: Dict[str, Any]) -> Dict[str, Any]:
         hash_password = self.hasher.hash(password)
         new_user["password"] = hash_password
         return new_user
 
-    async def create_user_and_formated(self, new_user: Dict[str, Any]) -> UserRead:
+    async def _create_user_and_formated(self, new_user: Dict[str, Any]) -> UserRead:
         user = await self.user_repo.create(new_user)
-        return UserRead(**user)
+        return UserRead.model_validate(user)
