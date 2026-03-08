baa76e46070e1bba7e1fc10ac812cc868809054d
gfedorov7 <fedorov.programming@gmail.com>
2026-03-08 17:53:16 +0300

    update hasher

--- src/util/hasher/bcrypt_hasher.py
+++ src/util/hasher/bcrypt_hasher.py
@@ -8,7 +8,7 @@ from src.util.hasher.hasher import Hasher
 class BcryptHasher(Hasher):
     @staticmethod
     def hash(data: Any) -> bytes:
-        return bcrypt.hashpw(bytes(data), BcryptHasher._get_salt())
+        return bcrypt.hashpw(data.encode(), BcryptHasher._get_salt())
 
     @staticmethod
     def compare(hash_value: bytes, value: Any) -> Any:
